d0a5040f3111b7158d9a890ea58061d6869fa9d0
asa <interchange1@foxmail.com>
2026-07-24 21:15:44 +0800

    import title

--- data/scenes/title/data.c
+++ data/scenes/title/data.c
@@ -8,83 +8,29 @@
 
 
 // [D_089dcf68] Title Logo Characters
-#ifdef PARADISE
 struct TitleLogoCharData title_logo_char_data[TOTAL_TITLE_LOGO_BUBBLES] = {
-    /* RH */ {
-        /* Anim. */ anim_title_logo_ri,
-        /* X, Y  */ 76, 70,
-        /* Angle */ 157
-    },
-    /* YT */ {
+    /* ZU */ {
         /* Anim. */ anim_title_logo_zu,
-        /* X, Y  */ 116, 62,
-        /* Angle */ -60
+        /* X, Y  */ 65, 84,
+        /* Angle */ 84
     },
-    /* HM */ {
+    /* MU */ {
         /* Anim. */ anim_title_logo_mu,
-        /* X, Y  */ 157, 54,
-        /* Angle */ -47
+        /* X, Y  */ 105, 75,
+        /* Angle */ 192
     },
-    /* HE */ {
+    /* TEN */ {
         /* Anim. */ anim_title_logo_ten,
-        /* X, Y  */ 87, 98,
-        /* Angle */ -133
+        /* X, Y  */ 139, 84,
+        /* Angle */ 44
     },
-    /* AV */ {
-        /* Anim. */ anim_title_logo_av,
-        /* X, Y  */ 129, 90,
-        /* Angle */ 97
-    },
-    /* EN */ {
+    /* GOKU */ {
         /* Anim. */ anim_title_logo_goku,
-        /* X, Y  */ 165, 81,
-        /* Angle */ -12
-    },
-    /* ADVANCE */ {
-        /* Anim. */ anim_title_logo_advance,
-        /* X, Y  */ 128, 91,
-        /* Angle */ 37
+        /* X, Y  */ 179, 75,
+        /* Angle */ -20
     }
 };
-#else
-struct TitleLogoCharData title_logo_char_data[TOTAL_TITLE_LOGO_BUBBLES] = {
-    /* RH */ {
-        /* Anim. */ anim_title_logo_ri,
-        /* X, Y  */ 75, 70,
-        /* Angle */ 157
-    },
-    /* YT */ {
-        /* Anim. */ anim_title_logo_zu,
-        /* X, Y  */ 116, 62,
-        /* Angle */ -60
-    },
-    /* HM */ {
-        /* Anim. */ anim_title_logo_mu,
-        /* X, Y  */ 157, 52,
-        /* Angle */ -47
-    },
-    /* HE */ {
-        /* Anim. */ anim_title_logo_ten,
-        /* X, Y  */ 93, 100,
-        /* Angle */ -133
-    },
-    /* AV */ {
-        /* Anim. */ anim_title_logo_av,
-        /* X, Y  */ 129, 91,
-        /* Angle */ 97
-    },
-    /* EN */ {
-        /* Anim. */ anim_title_logo_goku,
-        /* X, Y  */ 159, 81,
-        /* Angle */ -12
-    },
-    /* ADVANCE */ {
-        /* Anim. */ anim_title_logo_advance,
-        /* X, Y  */ 128, 91,
-        /* Angle */ 37
-    }
-};
-#endif
+
 
 // [D_089dcfa4] Graphics Table
 struct GraphicsTable title_gfx_table[] = {
--- graphics/title/title_graphics.h
+++ graphics/title/title_graphics.h
@@ -15,20 +15,14 @@ extern struct Animation anim_title_text[];
 extern struct Animation anim_title_null1[];
 extern struct Animation anim_title_stars_spin[];
 extern struct Animation anim_title_stars_appear[];
-extern struct Animation anim_title_logo_ri[];
 extern struct Animation anim_title_logo_zu[];
 extern struct Animation anim_title_logo_mu[];
 extern struct Animation anim_title_logo_ten[];
 extern struct Animation anim_title_logo_goku[];
-extern struct Animation anim_title_logo_av[];
-extern struct Animation anim_title_logo_advance[];
-extern struct Animation anim_empty[];
-extern struct Animation anim_empty_2[];
 extern struct Animation anim_title_logo_bubble_inner[];
 extern struct Animation anim_title_logo_bubble_outer[];
 extern struct Animation anim_title_logo_test[];
 extern struct Animation anim_title_shooting_star[];
-extern const u8 CD7948_titlescreen_obj_bin;
 
 /* TITLE - COMPRESSED GRAPHICS */
 
