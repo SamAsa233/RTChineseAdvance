e4d1053c58eaa4c915192ec3100987de49b0ab88
asa <interchange1@foxmail.com>
2026-09-21 17:32:06 +0800

    cedits_ver1

--- games/staff_credit/staff_credit_text.c
+++ games/staff_credit/staff_credit_text.c
@@ -5,155 +5,155 @@
 /* Script Data - Staff Credits */
 
 
-const char D_08069a48[] = "\0023" "Staff Credits";
+const char D_08069a48[] = "\0023" "制作人员";
 
-const char D_08069a58[] = "\0023" "Producers";
+const char D_08069a58[] = "\0023" "制作人";
 
-const char D_08069a64[] = "Tsunku♂";
+const char D_08069a64[] = "つんく♂";
 
-const char D_08069a70[] = "Y. Sakamoto";
+const char D_08069a70[] = "坂本 賀勇";
 
-const char D_08069a7c[] = "\0023" "Supervisors";
+const char D_08069a7c[] = "\0023" "监修";
 
-const char D_08069a88[] = "K. Yamano";
+const char D_08069a88[] = "山野 勝也";
 
-const char D_08069a94[] = "H. Iida";
+const char D_08069a94[] = "飯田 寛";
 
-const char D_08069a9c[] = "N. Umemoto";
+const char D_08069a9c[] = "梅本 のぶやす";
 
-const char D_08069aa8[] = "\0023" "Chief Director";
+const char D_08069aa8[] = "\0023" "总监";
 
-const char D_08069ab8[] = "K. Osawa";
+const char D_08069ab8[] = "大澤 和義";
 
-const char D_08069ac0[] = "\0023" "Game Design";
+const char D_08069ac0[] = "\0023" "游戏设计";
 
-const char D_08069acc[] = "K. Takeuchi";
+const char D_08069acc[] = "竹内 高";
 
-const char D_08069ad8[] = "M. Yone";
+const char D_08069ad8[] = "米 政美";
 
-const char D_08069ae0[] = "S. Ikeo";
+const char D_08069ae0[] = "池尾 佐知子";
 
-const char D_08069ae8[] = "N. Furuta";
+const char D_08069ae8[] = "古田 律克";
 
-const char D_08069af4[] = "R. Takahashi";
+const char D_08069af4[] = "高橋 龍太郎";
 
-const char D_08069b00[] = "Y. Takeuchi";
+const char D_08069b00[] = "竹内 康貴";
 
-const char D_08069b0c[] = "M. Kawano";
+const char D_08069b0c[] = "河野 正博";
 
-const char D_08069b18[] = "\0023" "Program Director";
+const char D_08069b18[] = "\0023" "程序总监";
 
-const char D_08069b2c[] = "\0023" "Programming";
+const char D_08069b2c[] = "\0023" "程序";
 
-const char D_08069b38[] = "\0023" "Design Director";
+const char D_08069b38[] = "\0023" "美术总监";
 
-const char D_08069b48[] = "\0023" "Design";
+const char D_08069b48[] = "\0023" "美术";
 
-const char D_08069b50[] = "F. Miyamoto";
+const char D_08069b50[] = "宮本 文子";
 
-const char D_08069b5c[] = "\0023" "Sound Directors";
+const char D_08069b5c[] = "\0023" "音频总监";
 
-const char D_08069b6c[] = "H. Oguro";
+const char D_08069b6c[] = "小黒 薫輝";
 
-const char D_08069b74[] = "K. Kamada";
+const char D_08069b74[] = "鎌田 浩二";
 
-const char D_08069b80[] = "\0023" "Music";
+const char D_08069b80[] = "\0023" "音乐";
 
-const char D_08069b88[] = "\0023" "Music Arrangement";
+const char D_08069b88[] = "\0023" "编曲";
 
-const char D_08069b9c[] = "K. Yuasa";
+const char D_08069b9c[] = "湯浅 公一";
 
-const char D_08069ba4[] = "H. D. Suzuki";
+const char D_08069ba4[] = "鈴木 DAICHI 秀行";
 
-const char D_08069bb0[] = "K. Okubo";
+const char D_08069bb0[] = "大久保 薫";
 
-const char D_08069bb8[] = "D. Shiiba";
+const char D_08069bb8[] = "椎葉 大翼";
 
-const char D_08069bc4[] = "\0023" "Sound Effects";
+const char D_08069bc4[] = "\0023" "音效";
 
-const char D_08069bd4[] = "T. Fujii";
+const char D_08069bd4[] = "藤井 貴矢";
 
-const char D_08069bdc[] = "\0023" "Singers";
+const char D_08069bdc[] = "\0023" "演唱";
 
-const char D_08069be4[] = "Ami Tokito";
+const char D_08069be4[] = "時東　ぁみ";
 
-const char D_08069bf0[] = "Soshi Tanaka";
+const char D_08069bf0[] = "田中　総史";
 
-const char D_08069bfc[] = "\0023" "Voice";
+const char D_08069bfc[] = "\0023" "配音";
 
-const char D_08069c04[] = "N. Kitamura";
+const char D_08069c04[] = "北村 典子";
 
-const char D_08069c10[] = "M. Nomura";
+const char D_08069c10[] = "野村 真穂";
 
-const char D_08069c1c[] = "\0023" "Debug";
+const char D_08069c1c[] = "\0023" "测试";
 
-const char D_08069c24[] = "Y. Adachi";
+const char D_08069c24[] = "安達 悠平";
 
-const char D_08069c30[] = "N. Hashida";
+const char D_08069c30[] = "橋田 直樹";
 
-const char D_08069c3c[] = "S. Takada";
+const char D_08069c3c[] = "髙田 成雄";
 
-const char D_08069c48[] = "D. Nishikawa";
+const char D_08069c48[] = "西川 大輔";
 
-const char D_08069c54[] = "R. Sugo";
+const char D_08069c54[] = "西郷 すご";
 
-const char D_08069c5c[] = "S. Adachi";
+const char D_08069c5c[] = "足立 真吾";
 
-const char D_08069c68[] = "H. Sakaguchi";
+const char D_08069c68[] = "坂口 さかぐち";
 
-const char D_08069c74[] = "N. Watanabe";
+const char D_08069c74[] = "渡辺 紀之";
 
-const char D_08069c80[] = "H. Matsuda";
+const char D_08069c80[] = "松田 まつだ";
 
-const char D_08069c8c[] = "D. Morita";
+const char D_08069c8c[] = "森田 もりた";
 
-const char D_08069c98[] = "K. Umeda";
+const char D_08069c98[] = "梅田 うめだ";
 
-const char D_08069ca0[] = "Y. Nakai";
+const char D_08069ca0[] = "中井 なかい";
 
-const char D_08069ca8[] = "Y. Fujikawa";
+const char D_08069ca8[] = "藤川 容子";
 
-const char D_08069cb4[] = "K. Nishimoto";
+const char D_08069cb4[] = "西本 にしもと";
 
-const char D_08069cc0[] = "T. Akiho";
+const char D_08069cc0[] = "海保 あきほ";
 
-const char D_08069cc8[] = "J. Terai";
+const char D_08069cc8[] = "寺井 准";
 
-const char D_08069cd0[] = "A. Shimizubata";
+const char D_08069cd0[] = "清水端 しみずばた";
 
-const char D_08069ce0[] = "Y. Yamashita";
+const char D_08069ce0[] = "山下 やました";
 
-const char D_08069cec[] = "\0023" "Special Thanks";
+const char D_08069cec[] = "\0023" "特别鸣谢";
 
-const char D_08069cfc[] = "Y. Kuratsune";
+const char D_08069cfc[] = "鞍常 くらつね";
 
-const char D_08069d08[] = "R. Kaimoto";
+const char D_08069d08[] = "改元 竜太";
 
-const char D_08069d14[] = "H. Sakagami";
+const char D_08069d14[] = "坂上 博樹";
 
-const char D_08069d20[] = "R. Yoshitomi";
+const char D_08069d20[] = "吉冨 亮二";
 
-const char D_08069d2c[] = "S. Yamamoto";
+const char D_08069d2c[] = "山本 伸樹";
 
-const char D_08069d38[] = "T. Hotta";
+const char D_08069d38[] = "堀田 拓司";
 
-const char D_08069d40[] = "N. Matsumiya";
+const char D_08069d40[] = "松宮 信雄";
 
-const char D_08069d4c[] = "M. Mitsubach";
+const char D_08069d4c[] = "みつばち　まき";
 
-const char D_08069d5c[] = "A. Kenmochi";
+const char D_08069d5c[] = "剣持 あゆち";
 
-const char D_08069d68[] = "H. Nakano";
+const char D_08069d68[] = "中野 ひとみ";
 
-const char D_08069d74[] = "\0023" "You!";
+const char D_08069d74[] = "\0023" "还有你！";
 
 const char D_08069d7c[] = "\0023" "";
 
-const char D_08069d88[] = "\0023" "Executive Producers";
+const char D_08069d88[] = "\0023" "执行制作人";
 
-const char D_08069d98[] = "S. Iwata";
+const char D_08069d98[] = "岩田 聡";
 
-const char D_08069da0[] = "S. Terai";
+const char D_08069da0[] = "寺井 てらい";
 
 const char D_08069da8[] = "";
 
@@ -161,52 +161,53 @@ const char D_08069dac[] = "(C) 2006 Nintendo / J.P ROOM";
 
 const char D_08069dc8[] =
     "\n"    
-    "Hey, you did it!";
+    "辛苦了。";
 
 const char D_08069ddc[] =
-    "You worked hard out there!\n"
-    "So, do you think your sense\n"
-    "of rhythm has gotten a little\n"
-    "bit better now?";
+    "你真的很努力呢！\n"
+    "你的节奏感要是\n"
+    "变得更好了，\n"
+    "我会很开心的。";
 
 const char D_08069e24[] =
-    "You're not done yet, though!\n"
-    "You may have reached the\n"
-    "credits, but there's still more\n"
-    "to play!";
+    "已经通关的游戏，\n"
+    "往往是玩熟了以后\n"
+    "才最有意思，所以之后也\n"
+    "再拿出来玩玩看吧！";
 
 const char D_08069e78[] =
-    "But before all that,\n"
-    "let me show you all of\n"
-    "the people who worked\n"
-    "on the game.";
+    "后面还有一点内容，\n"
+    "不过先在这里\n"
+    "给你介绍一下\n"
+    "制作人员吧。";
 
 const char D_08069ed4[] =
     "\n"
-    "Well, here we go!";
+    "那么，请看吧〜！";
 
 const char D_08069ee8[] =
     "\n"
-    "Hey, welcome back!";
+    "你好呀！";
 
 const char D_08069ef8[] =
-    "Are you holding up well?\n"
-    "Is your sense of rhythm\n"
-    "still improving?\n"
-    "I sure hope so!";
+    "还在加油吗？\n"
+    "你的节奏感要是\n"
+    "变得更好了，\n"
+    "我会很开心的。";
 
 const char D_08069f40[] =
-    "You know, nothing's more fun\n"
-    "than mastering games.\n"
-    "You should keep replaying\n"
-    "them to get better!";
+    "已经通关的游戏，\n"
+    "往往是玩熟了以后\n"
+    "才最有意思，所以也要\n"
+    "多玩几次哦！";
 
 const char D_08069f90[] =
-    "Anyway, here's the game's\n"
-    "staff again.\n"
-    "I wonder what they're\n"
-    "working on now...";
+    "对了，还得介绍\n"
+    "游戏的制作人员呢。\n"
+    "不知道他们现在\n"
+    "又在做什么游戏呢…";
 
 const char D_08069fe4[] =
     "\n"
-    "Well, here we go!";
+    "好了，先不说这个。\n"
+    "那么，请看吧〜！";
